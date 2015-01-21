a1902663993a7cbb73c58a505358b25d64e7ae95
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2015-01-21 15:25:16 -0600

    camelize controllers

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,8 @@
-var HomeController      = require('../controllers/home_controller');
+var homeController      = require('../controllers/home_controller');
 
 module.exports = function(app, passport) {
 
-  app.get('/', HomeController.index);
+  app.get('/', homeController.index);
 
   app.get('/auth/facebook', passport.authenticate('facebook'));
   app.get('/auth/facebook/callback', passport.authenticate('facebook', {
