bfd274f87980e2a1cd90547d18ce2529f68fd7a4
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2015-01-21 03:01:46 -0600

    requiring passport

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var express   = require('express'),
-path          = require('path'),
-favicon       = require('serve-favicon'),
-logger        = require('morgan'),
-cookieParser  = require('cookie-parser'),
-bodyParser    = require('body-parser'),
-app           = express();
+var express       = require('express'),
+path              = require('path'),
+favicon           = require('serve-favicon'),
+logger            = require('morgan'),
+cookieParser      = require('cookie-parser'),
+bodyParser        = require('body-parser'),
+passport          = require('passport'),
+FacebookStrategy  = require('passport-facebook').Strategy;
+app               = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
