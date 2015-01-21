50d0694a9bd4e54b8395813b1363dce68e85b3f9
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2015-01-21 03:04:01 -0600

    logind routes handler

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,17 @@
-var HomeController = require('../controllers/home_controller');
+var HomeController      = require('../controllers/home_controller');
 
-module.exports = function(app) {
+module.exports = function(app, passport) {
 
   app.get('/', HomeController.index);
+
+  app.get('/auth/facebook', passport.authenticate('facebook'));
+  app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    successRedirect: '/',
+    failureRedirect: '/auth/fail'
+  }));
+
+  app.get('/logout',function(req, res){
+    res.redirect('/');
+  });
 
 };
