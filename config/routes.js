f2b6f7abfeb6177d54b3436df1699921aec93fd7
Alberto Alcaraz <jaalcarazz@yahoo.com>
2015-01-21 20:20:28 -0600

    Added new page where the chat should be, also inlcuded return statement

--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,7 @@ var homeController      = require('../controllers/home_controller');
 module.exports = function(app, passport) {
 
   app.get('/', homeController.index);
+  app.get('/chat', homeController.chat);
 
   app.get('/auth/facebook', passport.authenticate('facebook'));
   app.get('/auth/facebook/callback', passport.authenticate('facebook', {
