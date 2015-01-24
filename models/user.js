5bef08a22c3ff9e8ebef4a58f79c1c5cae4a7aa2
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2015-01-23 23:46:44 -0600

    create user model

---
+++ models/user.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose'),
+findOrCreate = require('mongoose-findorcreate');
+
+module.exports = mongoose.model('User', new mongoose.Schema({
+
+  name: {
+    type: String,
+    require: true
+  },
+
+  facebook_id: {
+    type: String,
+    require: true
+  },
+
+  profile_picture: {
+    type: String,
+    require: true
+  }
+
+})
+.plugin(findOrCreate));
