19b99cde8ec680edb71ae7a7dbec21f983875095
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2015-01-23 23:47:12 -0600

    configure passport to create user from facebook callback

--- config/passport.js
+++ config/passport.js
@@ -1,14 +1,25 @@
+var User = require('../models/user');
+
 module.exports = function(passport, FacebookStrategy, config) {
 
   passport.use(new FacebookStrategy({
     clientID: config.get('facebook.clientID'),
     clientSecret: config.get('facebook.clientSecret'),
     callbackURL: "http://localhost:3000/auth/facebook/callback",
+    profileFields: ['id', 'displayName', 'photos']
   },
 
   function(accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
-      return done(null, profile);
+      User.findOrCreate({
+        name: profile.displayName,
+        facebook_id: profile.id,
+        profile_picture: profile.photos[0].value
+      },
+      function(err, user){
+        debugger
+        return done(null, user);
+      });
     });
   }
 
