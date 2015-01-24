7132c1cf80103226204dc4d078e5617d40374771
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2015-01-24 17:06:54 -0600

    configure to use bower to manage assets

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use(session({
     store: new RedisStore(),
     secret: 'node chat application',
